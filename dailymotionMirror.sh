51f1fa1a07e07b9ee0b9b805366b2d19c232df46
skytunnel <32209413+skytunnel@users.noreply.github.com>
2018-11-13 21:42:04 +0000

    Update dailymotionMirror.sh

--- dailymotionMirror.sh
+++ dailymotionMirror.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Version Tracking
-scriptVersionNo=0.2.2
+scriptVersionNo=0.2.3
 
 # Error handler just to print where fault occurred.  But code will still continue
 errorHandler() {
@@ -3212,7 +3212,7 @@ updateSourceCode() {
     fi
     
     # Update youtube-dl as well
-    sudo %ytdl --update
+    sudo $ytdl --update
     
     # Success
     echo ""
